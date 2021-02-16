6218560261b5da7e085c130f570ad153e51c8f86
wesley mendonça <wesley.mensilva18@gmail.com>
2021-02-16 13:51:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const header = document.querySelector('header');
+
+// header.setAttribute('id', 'header');
+
+// const headerID = document.querySelector('#header');
+
+// console.log(headerID)
+
+
+// const input = document.querySelector('input');
+
+// input.addEventListener('keypress', function(event) {
+//     console.log(event.key)
+// })
